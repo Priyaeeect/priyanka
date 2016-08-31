44d7055a220df72ddc89281c17153948d44360e7
Priyaeeect <priyaeeect@gmail.com>
2016-08-31 18:25:33 +0530

    Update index.java

--- index.java
+++ index.java
@@ -3,14 +3,14 @@ public class index
 {
 public static void main(String[] args)
 {
-Scanner s=new Scanner(System.in);
-int n=s.nextInt();
-int a[]=new int[n];
-for(int i=0;i<n;i++)
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int a[]=new int[n1];
+for(int i=0;i<n1;i++)
 {
-a[i]=s.nextInt();
+a[i]=sc.nextInt();
 }
-for(int i=0;i<n;i++)
+for(int i=0;i<n1;i++)
  {
  if(a[i]==i)
   {
