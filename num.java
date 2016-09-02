699a645ea863f8527a4772353671385b5486e6b6
Priyaeeect <priyaeeect@gmail.com>
2016-09-02 10:54:49 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class num
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n!=0)
+{
+if(n%2==0)
+{
+System.out.println("number is even");
+}
+else
+{
+System.out.println("number is odd");
+}
+}
+else
+System.out.println("number is zero");
+}
+}
