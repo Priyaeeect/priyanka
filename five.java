b8c6e3dcd67c7a25f89f3fbbc523ddb08a34599b
Priyaeeect <priyaeeect@gmail.com>
2016-09-02 11:16:39 +0530

    Update five.java

--- five.java
+++ five.java
@@ -3,7 +3,8 @@ class five
 {
 public static void main(String args[])
 {
-String s="Hello";
+  Scanner sc=new Scanner(System.in); 
+String s=sc.next();
 for(int i=0;i<5;i++)
 {
 System.out.println(s);
