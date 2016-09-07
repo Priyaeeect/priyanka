74dafa281b4432407f4e2f83b1677a68e7a6c31d
Priyaeeect <priyaeeect@gmail.com>
2016-09-07 12:15:00 +0530

    Create camel.java

---
+++ camel.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class camel{
+public static void main(String args[]){
+String s="priyanka suresh";
+String z[]=s.split(" ");
+for(int j=0;j<z.length;j++)
+{
+String v=z[i].substring(0,1).toUpperCase()+z[i].substring(1);
+System.out.print(v+" ");
+}
+}}
